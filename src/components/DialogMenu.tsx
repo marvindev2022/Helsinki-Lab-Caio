9ca946dca404fef4593ac3cc2663c60d2b2797e2
Marcus  Roza <mavirolero@gmail.com>
2023-11-30 15:06:55 -0300

    ajuste ul

--- src/components/DialogMenu.tsx
+++ src/components/DialogMenu.tsx
@@ -16,22 +16,22 @@ export default function RenderDialog({
 }: PropsDialog) {
   return (
     <motion.dialog
-    initial="hidden"
-    animate="visible"
-    variants={variants}
+      initial="hidden"
+      animate="visible"
+      variants={variants}
       transition={{ duration: 1, delay: 0.5 }}
       exit={{ opacity: 0.5 }}
       className=" min-w-[100vw] min-h-[100vh] sm:hidden bg-white  z-20  overflow-hidden"
     >
       <div className="bg-white min-w-[100vw] w-screen h-screen flex flex-col justify-around items-center rounded-xl relative ">
-        <span
-          className="w-full px-5 text-black  font-[special] flex justify-between text-4xl cursor-pointer"
-        >
+        <span className="w-full px-5 text-black  font-[special] flex justify-between text-4xl cursor-pointer">
           <h2>Caio Graco</h2>
-          <h2 
-          onClick={toggleMobileMenu}
-          className=" font-bold text-4xl cursor-pointer font-[special]"
-          >X</h2>
+          <h2
+            onClick={toggleMobileMenu}
+            className=" font-bold text-4xl cursor-pointer font-[special]"
+          >
+            X
+          </h2>
         </span>
         <ul className="flex h-1/2 justify-center items-center flex-col gap-5 text-4xl">
           {renderNavLinks()}
