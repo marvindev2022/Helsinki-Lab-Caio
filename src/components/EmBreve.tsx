1f34273c8edc75052ac231a9a13b5e06741d9822
Marcus  Roza <mavirolero@gmail.com>
2024-01-13 23:57:29 -0300

    Add EmBreve component

---
+++ src/components/EmBreve.tsx
@@ -0,0 +1,13 @@
+
+export default function RenderConstructor() {
+  return (
+    <section className="w-screen h-screen flex justify-center items-center bg-gray-400">
+      <h1
+       
+        className="text-4xl text-center text-black font-bold"
+      >
+        Em Construção
+      </h1>
+    </section>
+  );
+}
